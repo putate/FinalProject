acd898ffa8d25627ab3dfd4d6c807291f182e72b
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-02-16 09:53:39 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 	   $(".IMG3").hide();
            $(".IMG1").fadeToggle();
    });
-   $(".slide").click(function(){      
-    $(".panel").slideToggle("slow");
+   $("#directions").click(function(){      
+    $("#words").slideToggle("slow");
   });
 });
 
