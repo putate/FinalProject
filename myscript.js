95259e6c46e1bbcc9980237b266fc445aeb6aa1e
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-02-15 20:54:00 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
    $(".IMG2").hide();
    $(".IMG3").hide();
    $(".IMG1").click(function() {
-	   $("#IMG1A").hide();
+	   $(".IMG1").hide();
            $(".IMG2").fadeToggle();
    });
    $(".IMG2").click(function(){
-	   $("#IMG2A").hide();
+	   $(".IMG2").hide();
            $(".IMG3").fadeToggle();
    });
    $(".IMG3").click(function(){
-	   $("#IMG3A").hide();
+	   $(".IMG3").hide();
            $(".IMG1").fadeToggle();
    });
 });
